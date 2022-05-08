9870a5dafcc0a763af6b023cb9f2df81bac77af3
anasdeyra <anasdoura607@gmail.com>
2022-05-08 12:13:50 +0100

    animation removed

--- pages/index.js
+++ pages/index.js
@@ -63,55 +63,37 @@ export default function Home({}) {
 
   return (
     <Stack spacing={"xl"} className={classes.container}>
-      <Transition
-        mounted={mounted}
-        transition="slide-up"
-        duration={1000}
-        timingFunction="ease"
-      >
-        {(styles) => (
-          <Stack py={"xl"} style={styles}>
-            <Title className={classes.title}>
-              Create, sell, buy, collect magnificent NFTs
-            </Title>
+      <Stack py={"xl"} style={styles}>
+        <Title className={classes.title}>
+          Create, sell, buy, collect magnificent NFTs
+        </Title>
 
-            <Group sx={{ zIndex: "1" }} mt={"xl"} spacing={"xl"}>
-              <Link href={"/marketplace"}>
-                <Button
-                  gradient={{ from: "indigo", to: "cyan" }}
-                  variant="gradient"
-                  size="xl"
-                >
-                  Explore
-                </Button>
-              </Link>
-              <Link href={"/my-nfts"}>
-                <Button variant="outline" size="xl">
-                  Create
-                </Button>
-              </Link>
-            </Group>
-          </Stack>
-        )}
-      </Transition>
+        <Group sx={{ zIndex: "1" }} mt={"xl"} spacing={"xl"}>
+          <Link href={"/marketplace"}>
+            <Button
+              gradient={{ from: "indigo", to: "cyan" }}
+              variant="gradient"
+              size="xl"
+            >
+              Explore
+            </Button>
+          </Link>
+          <Link href={"/my-nfts"}>
+            <Button variant="outline" size="xl">
+              Create
+            </Button>
+          </Link>
+        </Group>
+      </Stack>
 
-      <Transition
-        mounted={mounted}
-        transition="slide-up"
-        duration={1000}
-        timingFunction="ease"
-      >
-        {(styles) => (
-          <Image
-            style={styles}
-            className={classes.image}
-            src="/zombie-hand.webp"
-            alt="illustration"
-            width={"615"}
-            height={"912"}
-          />
-        )}
-      </Transition>
+      <Image
+        style={styles}
+        className={classes.image}
+        src="/zombie-hand.webp"
+        alt="illustration"
+        width={"615"}
+        height={"912"}
+      />
 
       <Stack className={classes.featured}>
         <Title>Featured</Title>
