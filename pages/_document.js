82e6eb7f3acc6db7cdf2b1b887c9573bd64a359c
anasdeyra <anasdoura607@gmail.com>
2022-05-09 12:03:20 +0100

    ssr styles fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,8 @@
-import Document, { DocumentContext } from "next/document";
-import { createStylesServer, ServerStyles } from "@mantine/ssr";
+import Document from "next/document";
+import { createGetInitialProps } from "@mantine/next";
+
+const getInitialProps = createGetInitialProps();
 
 export default class _Document extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-
-    // Add your app specific logic here
-    const stylesServer = createStylesServer();
-    console.log(
-      <>
-        {initialProps.styles}
-        <ServerStyles html={initialProps.html} server={stylesServer} />
-      </>
-    );
-
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          <ServerStyles html={initialProps.html} server={stylesServer} />
-        </>
-      ),
-    };
-  }
+  static getInitialProps = getInitialProps;
 }
